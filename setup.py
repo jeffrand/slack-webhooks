ba9a7a2f5ab2ef33703c5d417cfc6eeba9918d41
Jeff Rand <jeff@betaworks.com>
2015-06-16 17:07:59 -0400

    bleg
    Signed-off-by: Jeff Rand <jeff@betaworks.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from pip.req import parse_requirements
-import uuid
-import os
-
-path = os.path.realpath(__file__)
-req_path = os.path.join(os.path.dirname(path), 'requirements.txt')
-
-install_reqs = parse_requirements(req_path, session=uuid.uuid1())
 
 setup(name='slack_webhooks',
-      version='1.0.2',
+      version='1.0.3',
       description='Webhook helper for Slack',
       author='Jeff Rand',
       author_email='jeffreyrand@gmail.com',
-      install_requires=[str(ir.req) for ir in install_reqs],
+      install_requires=[
+        'requests==2.7.0',
+      ],
       url='https://github.com/jeffrand/slack-webhooks',
       packages=['slack_webhooks'],
 )
